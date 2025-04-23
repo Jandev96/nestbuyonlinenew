2726d13c37c659b226b47737c7b14609ebc01ce4
JANDEV <jandev66@gmail.com>
2025-04-22 17:12:25 -0700

    footer updated

--- src/components/user/Footer.jsx
+++ src/components/user/Footer.jsx
@@ -6,10 +6,10 @@ function Footer() {
     <div>
         <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
   <nav className="grid grid-flow-col gap-4">
-    <Link to='/about' className="link link-hover">About us</Link>
+    <Link to='/' className="link link-hover">Home</Link>
     <Link to='/contact' className="link link-hover">Contact</Link>
     <Link to='/products' className="link link-hover">Store</Link>
-    <Link   className="link link-hover">About us</Link>
+    <Link to='/about'  className="link link-hover">About us</Link>
 
 
    
